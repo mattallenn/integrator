02397ae36d6bc187a19e6a61813d85fecfd897df
mattallenn <104325813+mattallenn@users.noreply.github.com>
2023-05-04 09:03:01 -0500

    started added lambda functionality

--- integrator.py
+++ integrator.py
@@ -1,5 +1,3 @@
-
-
 n = 0.01#Number of trapezoids to integrate aka base
 n1 = 0 
 n2 = n 
@@ -8,10 +6,8 @@ lower_bount = 0
 func = "2x+1"
 area = 0
 
-while n2 <= upper_bound:
-    area += 0.5((2(n1)+1) + (2(n2)+1))
-    n1 += n
-    n2 += n
+func = lambda x: eval(input())
+print(func)
 
-print(area) 
+
 
