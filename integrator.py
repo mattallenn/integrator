2bc0b0bd3694fd2f774ee52b489756e0f1971e06
mattallenn <matthewallen442@gmail.com>
2023-05-05 22:01:13 -0500

    got trapezoidal rule working

--- integrator.py
+++ integrator.py
@@ -1,28 +1,43 @@
 import math
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
-n = 0.01#Number of trapezoids to integrate aka base
-n1 = 0 
-n2 = n 
-upper_bound = 2
-lower_bount = 0
-area = 0
+# Get user input for function to integrate
+function_str = input("Enter a function in terms of x. (ex. 2*x+1) \nMake sure to use valid Python syntax. Type help for syntax list.\n")
+if function_str == "help":
+    print('x squared(or to another power) = x**2\nfor trig functions, use the python math library syntax.\n sin(x) = math.sin(x), cos(x) = math.cos(x), etc.')
+else:
+    # Convert user input function string to a callable function using eval()
+    def user_function(x):
+        return eval(function_str)
 
-''' while n2 <= upper_bound:
-    area += 0.5((2(n1)+1) + (2(n2)+1))
-    n1 += n
-    n2 += n
+    # Get user input for integration bounds and number of trapezoids
+    a = float(input("Enter lower bound: "))
+    b = float(input("Enter upper bound: "))
+    n = int(input("Enter number of trapezoids: "))
 
-print(area) 
-'''
+    # Calculate width of each trapezoid
+    h = (b - a) / n
 
-try:
-    function_str = input("Enter a function in terms of x. (ex. 2*x+1) \nMake sure to use valid Python syntax. Type help for syntax list.\n")
-    if function_str == "help":
-        print('x squared(or to another power) = x**2\nfor trig functions, use the python math library syntax.\n sin(x) = math.sin(x), cos(x) = math.cos(x), etc.')
-except:
-    print("Invalid syntax or equation.")
-def function(x):
-    return eval(function_str)
+    fig, ax = plt.subplots()
 
-x = 2
-print(function(x))
+    #create simple line plot
+    ax.plot([0, 100],[0, 100])
+
+
+    # Calculate area using trapezoidal rule
+    area = 0.5 * (user_function(a) + user_function(b))
+    for i in range(1, n):
+        x = a + i * h
+        area += user_function(x)
+    area *= h
+
+    # Print the result
+    print("The numerical integration of the function is: {:.6f}".format(area))
+
+
+#add rectangle to plot
+ax.add_patch(Rectangle((1, 1), 2, 6))
+
+#display plot
+plt.show()
